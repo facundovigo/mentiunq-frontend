e8cbe8918e8c6c150644af3717341c45d5f1fc2d
facundo <facundovigo@gmail.com>
2022-09-05 18:27:38 -0300

    test heroku

--- server.js
+++ server.js
@@ -5,14 +5,12 @@ const path = require('path');
 
 const app = express();
 
-//app.use(serveStatic(__dirname + "/dist"));
-//here we are configuring dist to serve app files
+//we are configuring dist to serve site files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
-
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
-	res.sendFile(path.join(__dirname, '/dist/index.html'))
+ res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
-
-var port = process.env.PORT || 5000;
-app.listen(port);
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log(`site is listening on port: ${port}`)
